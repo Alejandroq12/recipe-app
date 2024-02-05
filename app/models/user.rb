2643579a0062232540d35b1edff14bf3b0800fce
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-05 15:45:44 -0600

    Feat: add relationship that shows that a user may have many recipes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :recipes
 end
+# rails g model Recipe name:string preparation_time:integer cooking_time:integer description:text public:boolean user:references
