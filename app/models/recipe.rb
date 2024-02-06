f23d687f855fd6c0e8514ea5e51125858a785902
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-06 12:59:26 -0600

    Refactor: recipe model to correctly erase associations and to implement the :recent_recipes method.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+
+  scope :recent_public, -> { where(public: true).order(created_at: :desc) }
 end
