d900b5809f0b3c39da93df0cb387b794452c405f
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-06 12:54:17 -0600

    Feat: add the ability file to set authorizations.

---
+++ app/models/ability.rb
@@ -0,0 +1,10 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    can :read, Recipe, public: true
+    can :manage, Recipe, user_id: user.id
+  end
+end
