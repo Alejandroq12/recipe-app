0147835be48ae881b673043d31eb827c4662a71a
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-07 12:30:47 -0600

    Feat: add validations and set correct relashionships to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
+  has_many :foods, dependent: :destroy
+
+  validates :name, presence: true
 end
