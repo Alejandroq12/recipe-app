45ceea8e24f88f0e071a99eec6939b7e6e4f3e72
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-10 19:30:14 -0600

    Update: production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
     domain: mailertogo_domain,
     user_name: mailertogo_user,
     password: mailertogo_password,
-    authentication: :plain,
+    authentication: :login,
     enable_starttls_auto: true
   }
   
